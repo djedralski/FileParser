0465fb801615865bc99c570c38389cd815064e30
Dariusz Jedralski <djedralski@gmail.com>
2017-12-08 00:52:46 -0600

    Updated Console App to actually follow the requirements

--- FileParser.App/Program.cs
+++ FileParser.App/Program.cs
@@ -1,7 +1,9 @@
 ﻿using FileParser.Code;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 
 namespace FileParser.App
 {
@@ -9,35 +11,61 @@ namespace FileParser.App
     {
         static void Main(string[] args)
         {
-            List<string> fileInput = new List<string>();
-            string path = null;
-            if (args == null || args.Length != 1)
+            //argument checks
+            if (args.Length != 3)
             {
-                path = Directory.GetCurrentDirectory() + @"\test.csv";
-            }
-            else
-            {
-                path = args[0];
-            }
-            if(!File.Exists(path))
-            {
-                //no file exists
-                Console.WriteLine("Requested File Does not Exist");                
+                Console.WriteLine("3 filepaths required");
                 return;
             }
-            using (var reader = new StreamReader(path))
+            //validate arguments lead to files
+            foreach (var arg in args)
             {
-                //Parse file
-                while (!reader.EndOfStream)
+                if (!File.Exists(arg))
                 {
-                    var line = reader.ReadLine();
-                    fileInput.Add(line);                   
-                }                
+                    Console.WriteLine($"No file exists at {arg}");
+                    return;
+                }
             }
-            //Convert to Record List
-            var parsedRecordLines = RecordParser.ParseLines(fileInput);
 
-            //Save Data Here
+            //Add records for each file to RecordList
+            var recordList = new List<Record>();
+            foreach (var arg in args)
+            {
+                List<string> fileInput = new List<string>();
+                using (var reader = new StreamReader(arg))
+                {
+                    //Parse file
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        fileInput.Add(line);
+                    }
+                }
+                recordList.AddRange(RecordParser.ParseLines(fileInput));
+            }
+
+            //Output 1 – sorted by gender(females before males) then by last name ascending.            
+            Console.WriteLine("Output 1 – sorted by gender(females before males) then by last name ascending");
+            recordList = recordList.OrderBy(x => x.Gender).ThenBy(x => x.LastName).ToList();
+            OutputRecordsToConsole(recordList);
+
+            //Output 2 – sorted by birth date, ascending.
+            Console.WriteLine("Output 2 – sorted by birth date, ascending.");
+            recordList = recordList.OrderBy(x => x.DateOfBirth).ToList();
+            OutputRecordsToConsole(recordList);
+
+            //Output 3 – sorted by last name, descending.
+            Console.WriteLine("Output 3 – sorted by last name, descending.");
+            recordList = recordList.OrderByDescending(x => x.LastName ).ToList();
+            OutputRecordsToConsole(recordList);
+
+        }
+        private static void OutputRecordsToConsole(List<Record> list)
+        {
+            foreach(var line in list)
+            {
+                Console.WriteLine($"{line.LastName}, {line.FirstName}, {line.Gender}, {line.FavoriteColor}, {line.DateOfBirth.ToString("M/d/yyyy", CultureInfo.InvariantCulture)} ");
+            }
         }
     }
 }
